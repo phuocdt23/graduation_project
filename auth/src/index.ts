618e937b3465216bc6f74c2b58ed697b98b46fed
phuocdt23 <phuoc.dt182724@sis.hust.edu.vn>
2022-12-05 09:22:57 -0500

    Ingress-Nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
 app.listen(3000, () => {
-  console.log('Auth listening on port 3000')
+  console.log('Auth listening on port 3000!!!!!!!!!!!')
 })
