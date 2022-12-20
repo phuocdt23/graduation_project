af8616d0901d505d224c102429656bf44708697c
phuocdt23 <phuoc.dt182724@sis.hust.edu.vn>
2022-12-20 17:13:56 +0700

    refactor: ENV variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,15 +11,27 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI must be defined');
     }
-    
-    await natsWapper.connect('ticketing', 'randomstring', 'http://nats-srv:4222');
+    if (!process.env.NATS_URL) {
+      throw new Error('NATS_URL must be defined');
+    }
+    if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error('NATS_CLUSTER_ID must be defined');
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+      throw new Error('NATS_CLIENT_ID must be defined');
+    }
+
+    await natsWapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL);
     natsWapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
     });
     process.on('SIGINT', () => natsWapper.client.close());
     process.on('SIGTERM', () => natsWapper.client.close());
-    
+
     mongoose.set('strictQuery', true)
     await mongoose.connect(process.env.MONGO_URI)
       .then(() => {
