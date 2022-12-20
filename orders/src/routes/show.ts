7f3f005ed3db3a690708656601d0b76aca0a480a
phuocdt23 <phuoc.dt182724@sis.hust.edu.vn>
2022-12-20 19:31:27 +0700

    feat: complete CRUD order

---
+++ orders/src/routes/show.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  UnauthorizedError,
+} from "@phuoc.dt182724/common";
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+
+    res.send(order);
+  }
+);
+
+export { router as showOrderRouter };
