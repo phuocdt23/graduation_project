507c5483150bd2faf2a1a0dd013eb31b6b0d4963
phuocdt23 <phuoc.dt182724@sis.hust.edu.vn>
2022-12-24 22:53:16 -0500

    refactor: tickets service

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,5 +1,5 @@
-import { Publisher, Subjects } from '@phuoc.dt182724/common';
-import { TicketCreatedEvent } from '@phuoc.dt182724/common';
+import { Publisher, Subjects, TicketCreatedEvent } from "@phuoc.dt182724/common";
+
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
 }
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,11 +1,10 @@
-import mongoose from "mongoose";
-import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
-  // status: string;
 }
 
 interface TicketDoc extends mongoose.Document {
@@ -14,52 +13,46 @@ interface TicketDoc extends mongoose.Document {
   userId: string;
   version: number;
   orderId?: string;
-  // status: string;
-  // createdAt: Date;
-  // updatedAt: Date;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
-const ticketSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    orderId: {
+      type: String,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-  orderId: {
-    type: String,
-  },
-},
   {
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
         delete ret._id;
-      }
-    }
+      },
+    },
   }
 );
-
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
-}
+};
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
 export { Ticket };
-
-
-
